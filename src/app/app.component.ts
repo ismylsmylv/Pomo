47a9d5ce738522c3eb79dcf8b68c61b13f2bf05e
ismylsmylv <ismayilovsmyl2020@gmail.com>
2024-08-31 13:50:33 +0400

    feat:components added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SwitchComponent } from './switch/switch.component';
+import { TimeComponent } from './time/time.component';
+import { ControlsComponent } from './controls/controls.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SwitchComponent, TimeComponent, ControlsComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Pomo';
